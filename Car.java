f90781d52cb7d68f3f64afddd0637548c5c11fb6
aibolataibekuly2007 <aibolataibekuly2007@gmail.com>
2026-01-28 10:22:25 +0500

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,18 @@
+public class Car extends Vehicle {
+    private int doors;
+    private String transmission;
+
+    public Car(String brand, String model, int year, int doors, String transmission) {
+        super(brand, model, year);
+        this.doors = doors;
+        this.transmission = transmission;
+    }
+
+    @Override
+    public String getInfo() {
+        return super.getInfo() +
+                ", двери: " + doors +
+                ", трансмиссия: " + transmission;
+    }
+}
+
